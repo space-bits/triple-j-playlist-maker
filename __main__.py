59e92ab854f3cb4e40885f912dafbedea74ffb01
Rob <rlainewyllie@gmail.com>
2018-11-27 16:25:24 +1100

    Create method to authorise; Only auth if not authed

--- __main__.py
+++ __main__.py
@@ -1,23 +1,58 @@
 import urllib.request
+import os
+from json.decoder import JSONDecodeError
 
 import spotipy
 import spotipy.util as util
 
 
 def main():
-	username = input('Enter spotify username:')
-	if username is None:
+	username = '011011000110111101110110011001'
+	sp = authenticate_account(username)
+	if sp is None:
 		return
 
-	token = util.prompt_for_user_token(username)
-	if not token or token is None:
-		return
-		
-	sp = spotipy.Spotify(auth=token)	
+	# save the user id for creating a playlist and modifying it
+	user_id = None
+
+	# create the playlist, if it doesn't exist	
+	create_playlist()
 	
-	results = sp.search(q='weezer', limit=20)
-	for i, t in enumerate(results['tracks']['items']):
-		print(' ', i, t['name'])
+def create_playlist():
+	'''Determine if there is already a playlist, else create one'''
+	playlist = None
+	return playlist
+
+def find_playlist(username, playlist_name):
+	'''Query the users playlist for if one already exists'''
+	playlist_id = None
+	
+	playlists = sp.user_playlists(username)
+	for playlist in playlists['items']:
+		if playlist['owner']['id'] == username:
+			print(playlist['id'])
+
+	return playlist_id
+
+def authenticate_account(username):
+	'''Authenticate the account and return the authenticated object.'''
+	if username is None:
+		username = input('Enter spotify username: ')
+	
+	scope = 'playlist-modify-public'
+	# try to get a new token or purge the old one if the scopes do not match	
+	try:
+		token = util.prompt_for_user_token(username, scope)
+	except (AttributeError, JSONDecodeError):
+		os.remove(f".cache-{username}")
+		token = util.prompt_for_user_token(username, scope)
+	
+	if not token or token is None:
+		print('Unable to get token for %s' % (username))
+		return None
+	
+	sp = spotipy.Spotify(auth=token)	
+	return sp
 
 if __name__ == '__main__':
     main()
